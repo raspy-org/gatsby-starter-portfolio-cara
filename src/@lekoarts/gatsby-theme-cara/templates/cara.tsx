711a11de96692be328a872c69cb4c5010a0a0c30
Ross Rasmussen <raspy8766@gmail.com>
2022-03-12 21:54:48 +0000

    Shadow all files

--- src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
+++ src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
@@ -1,10 +1,10 @@
 import * as React from "react"
 import { Parallax } from "@react-spring/parallax"
 import Layout from "../components/layout"
-import Hero from "../components/hero"
-import Projects from "../components/projects"
-import About from "../components/about"
-import Contact from "../components/contact"
+// import Hero from "../components/hero"
+// import Projects from "../components/projects"
+// import About from "../components/about"
+// import Contact from "../components/contact"
 
 const Cara = () => (
   <Layout>
