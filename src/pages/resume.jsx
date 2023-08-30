863645f0dddb503946840322ebfc19ff42c0936a
Ross <raspy8766@gmail.com>
2023-08-29 21:39:24 -0700

    Add resume

--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -1,24 +1,33 @@
-import Layout from "@lekoarts/gatsby-theme-cara/src/components/layout"
 import Seo from "@lekoarts/gatsby-theme-cara/src/components/seo"
-import Content from "@lekoarts/gatsby-theme-cara/src/elements/content"
-import Inner from "@lekoarts/gatsby-theme-cara/src/elements/inner"
-import { Parallax } from "@react-spring/parallax"
+import { Image, Flex } from 'theme-ui'
 import * as React from "react"
-import { Themed } from "theme-ui"
+import DarkModeToggle from '../@lekoarts/gatsby-theme-cara/components/dark-mode-toggle';
+import GlowingLinkButton from '../@lekoarts/gatsby-theme-cara/components/glowing-button-link';
+import Name from '../@lekoarts/gatsby-theme-cara/components/name';
 
-const NotFound = () => (
-  <Layout>
-    <Seo title="About" />
-    <Parallax pages={1}>
-      <div>
-        <Content sx={{ variant: `texts.bigger` }} speed={0.4} offset={0} factor={1}>
-          <Inner>
-            <img src='./resume.jpeg'/>
-          </Inner>
-        </Content>
-      </div>
-    </Parallax>
-  </Layout>
+const Resume = () => (
+  <Flex>
+    <Flex sx={{
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      position: 'absolute',
+      zIndex: 100,
+      height: 100,
+      width: '100%',
+      p: 20,
+    }}>
+      <Name>Ross Rasmussen</Name>
+      <Flex sx={{ gap: 30 }}>
+        <GlowingLinkButton to='/'>Home</GlowingLinkButton>
+        <GlowingLinkButton to='/resume'>Resume</GlowingLinkButton>
+        <DarkModeToggle></DarkModeToggle>
+    </Flex>
+    </Flex>
+    <Flex sx={{ overflow: 'auto', justifyContent: 'center', width: '100vw' }}>
+      <Seo title="Resume" />
+      <Image sx={{ p: '130px 0 50px 0', width: '60vw' }} src='/resume.jpeg'/>
+    </Flex>
+  </Flex>
 )
 
-export default NotFound
+export default Resume
